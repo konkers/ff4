710d747666e3f4faffcc48c371226d23400cb9df
Erik Gilling <konkers@konkers.net>
2019-01-12 16:42:56 -0500

    Add basic monster parsing.

---
+++ src/rom_map.rs
@@ -0,0 +1,22 @@
+pub const MONSTER_NAME_TABLE: usize = 0x71800;
+pub const MONSTER_NAME_TABLE_END: usize = 0x71f00;
+
+pub const MONSTER_GP_TABLE: usize = 0x72000;
+pub const MONSTER_GP_TABLE_ENTRIES: usize = 0xe0;
+
+pub const MONSTER_XP_TABLE: usize = 0x721C0;
+pub const MONSTER_XP_TABLE_ENTRIES: usize = 0xe0;
+
+pub const MONSTER_OFFSET_TABLE: usize = 0x726A0;
+pub const MONSTER_OFFSET_TABLE_ENTRIES: usize = 0xe0;
+
+pub const MONSTER_STAT_TABLE: usize = 0x72380;
+pub const MONSTER_STAT_TABLE_END: usize = 0x7261f;
+
+pub const MONSTER_SPEED_TABLE: usize = 0x72620;
+pub const MONSTER_SPEED_TABLE_END: usize = 0x7269f;
+
+pub const MONSTER_DROP_TABLE: usize = 0x71e00;
+pub const MONSTER_DROP_TABLE_END: usize = 0x71f00;
+
+pub const MONSTER_INFO_OFFSET: usize = 0x72860 - 0xa860;
